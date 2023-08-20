cdb003c0bc9483d695e5f813d02d2947111e9ba5
seoziyeong <bestzenio@gmail.com>
2023-08-20 16:04:48 +0900

    [feat] pricing - 반응형

--- src/pages/pricing/Pricing.js
+++ src/pages/pricing/Pricing.js
@@ -186,15 +186,20 @@ const Pricing = () => {
                 </div>
               </div>
             </div>
-            <p className="plan-guide">
+            <p className="plan-guide pc">
               상기 구독요금은 1인 기준의 월간 사용 요금으로, VAT(부가세)가
               포함되지 않은 금액입니다.
+            </p>
+            <p className="plan-guide m">
+              * 상기 구독요금은 1인 기준의 월간 사용 요금으로, VAT(부가세)가
+              포함되지 않은 금액입니다.
+              <br />* 상세 요금 비교 표는 데스크탑 버전으로 확인 부탁드립니다.
             </p>
           </article>
         </section>
       </div>
 
-      <div className="pricing-container">
+      <div className="pricing-container compare-wrapper">
         <section className="wrap-container width-1130">
           <hgroup className="title-group">
             <h1 className="title">구독요금 비교하기</h1>
