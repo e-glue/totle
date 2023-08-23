31e31a6c0c36365c636c91e64e5e6abacfa06b03
ChoHye <chohye0831@gmail.com>
2023-08-23 22:13:42 +0900

    [rename] introduction convention 적용  (#12)
    * [style] introduction - prettier 적용
    
    * [rename] introduction - banner 컴포넌트 파일명 수정
    
    * [rename] introduction - customer logo 파일명 수정
    
    ---------
    
    Co-authored-by: park cho hye <chohye@172.20.nate.com>

--- src/pages/introduction/Introduction.js
+++ src/pages/introduction/Introduction.js
@@ -1,7 +1,7 @@
 import "../../styles/styles.scss";
 import "./introduction.scss";
-import WistiaPlayer from './components/WistiaPlayer';
-import Banner from "./components/Banner"
+import WistiaPlayer from "./components/WistiaPlayer";
+import Banner from "./components/RollingListBanner";
 
 const Introduction = () => {
   return (
@@ -11,43 +11,54 @@ const Introduction = () => {
           <div className="introduce-table">
             <p className="text-top">아웃룩을 새롭게</p>
             <p className="text-bottom">더 가치 있게 </p>
-            <span>토틀은 Outlook에서 부족한 부분들을 채워주는<br/>
-            올인원 아웃룩 애드인 솔루션입니다.</span>
+            <span>
+              토틀은 Outlook에서 부족한 부분들을 채워주는
+              <br />
+              올인원 아웃룩 애드인 솔루션입니다.
+            </span>
           </div>
           <div className="email-table">
-            <input className="email-input" placeholder="이메일 주소를 입력해주세요."></input>
+            <input
+              className="email-input"
+              placeholder="이메일 주소를 입력해주세요."
+            ></input>
             <button className="email-button">계정 생성하기</button>
-            <p className="email-text">무료 계정을 생성해 토틀의 다양한 기능을 활용해보세요!</p>
+            <p className="email-text">
+              무료 계정을 생성해 토틀의 다양한 기능을 활용해보세요!
+            </p>
           </div>
-          <div className="subscribe">
-          → 토틀 구독하기
-          </div>
+          <div className="subscribe">→ 토틀 구독하기</div>
         </div>
         <div className="video-wrapper">
-        <WistiaPlayer
-          videoId="4pzjgbcafv"
-          wrapper="wistia-player-container-1"
-        />
+          <WistiaPlayer
+            videoId="4pzjgbcafv"
+            wrapper="wistia-player-container-1"
+          />
         </div>
       </div>
       <div className="product-wrapper">
         <div className="product-text">
           <p className="t1">Product Tour 👇</p>
           <p className="t2">
-            토틀의  
+            토틀의
             <span className="t2-1"> 주요 기능을</span>
             <span className="t2-2"> 직접 경험</span>
             해보세요!
           </p>
         </div>
         <div className="product-frame">
-          <iframe class="totle-storylane" src="https://totle.storylane.io/share/rsg6yhadwehm" allow="fullscreen; camera; microphone"></iframe>
+          <iframe
+            class="totle-storylane"
+            src="https://totle.storylane.io/share/rsg6yhadwehm"
+            allow="fullscreen; camera; microphone"
+          ></iframe>
         </div>
       </div>
 
       <div className="customer-wrapper">
         <div className="customer-text">
-          전세계 <span className="t1">165,000명 이상의</span><br/>
+          전세계 <span className="t1">165,000명 이상의</span>
+          <br />
           <span className="t2">개인 및 기업 사용자</span>가 사용 중 입니다.
         </div>
         <div className="customer-banner">
@@ -55,7 +66,7 @@ const Introduction = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Introduction;
