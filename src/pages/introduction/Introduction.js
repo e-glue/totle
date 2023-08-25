0bb101b6f5a11a1181233d33fc39754dde81ac08
park cho hye <chohye@172.20.nate.com>
2023-08-25 18:06:26 +0900

    [refactor] introduction - customer section 리팩토링

--- src/pages/introduction/Introduction.js
+++ src/pages/introduction/Introduction.js
@@ -2,20 +2,14 @@ import "../../styles/styles.scss";
 import "./introduction.scss";
 import SectionTop from "./components/section-top/SectionTop";
 import SectionProduct from "./components/section-product/SectionProduct";
+import SectionCustomer from "./components/section-customer/SectionCustomer";
 
 const Introduction = () => {
   return (
     <div className="wrap-container width">
       <SectionTop></SectionTop>
       <SectionProduct></SectionProduct>
-      <div className="customer-wrapper">
-        <div className="customer-text">
-          전세계 <span className="t1">165,000명 이상의</span>
-          <br />
-          <span className="t2">개인 및 기업 사용자</span>가 사용 중 입니다.
-        </div>
-        <div className="customer-banner">{/* <Banner></Banner> */}</div>
-      </div>
+      <SectionCustomer></SectionCustomer>
     </div>
   );
 };
--- src/pages/introduction/components/rolling-list-banner/RollingListBanner.js
+++ src/pages/introduction/components/rolling-list-banner/RollingListBanner.js
@@ -1,14 +1,14 @@
 import "./RollingListBanner.scss";
-import customer1 from "../../../assets/introduction/dgb-capital.png";
-import customer2 from "../../../assets/introduction/skc-infraservice.png";
-import customer3 from "../../../assets/introduction/unives.png";
-import customer4 from "../../../assets/introduction/millie.png";
-import customer5 from "../../../assets/introduction/siwonschool.png";
-import customer6 from "../../../assets/introduction/sk-broadband.png";
-import customer7 from "../../../assets/introduction/cosmax.png";
-import customer8 from "../../../assets/introduction/hankook-research.png";
-import customer9 from "../../../assets/introduction/kpac.png";
-import customer10 from "../../../assets/introduction/hyundai-lng.png";
+import customer1 from "../../../../assets/introduction/dgb-capital.png";
+import customer2 from "../../../../assets/introduction/skc-infraservice.png";
+import customer3 from "../../../../assets/introduction/unives.png";
+import customer4 from "../../../../assets/introduction/millie.png";
+import customer5 from "../../../../assets/introduction/siwonschool.png";
+import customer6 from "../../../../assets/introduction/sk-broadband.png";
+import customer7 from "../../../../assets/introduction/cosmax.png";
+import customer8 from "../../../../assets/introduction/hankook-research.png";
+import customer9 from "../../../../assets/introduction/kpac.png";
+import customer10 from "../../../../assets/introduction/hyundai-lng.png";
 
 const Banner = () => {
   return (
