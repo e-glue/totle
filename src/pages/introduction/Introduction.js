1cc773a92e4f3816b64b71f87101ec13d8c29a91
park cho hye <chohye@172.20.nate.com>
2023-08-25 17:42:07 +0900

    [refactor] introduction - top section 리팩토링

--- src/pages/introduction/Introduction.js
+++ src/pages/introduction/Introduction.js
@@ -1,41 +1,12 @@
 import "../../styles/styles.scss";
 import "./introduction.scss";
-import WistiaPlayer from "./components/WistiaPlayer";
+import SectionTop from "./components/section-top/SectionTop";
 import Banner from "./components/RollingListBanner";
 
 const Introduction = () => {
   return (
     <div className="wrap-container width">
-      <div className="top-wrapper">
-        <div className="title-wrapper">
-          <div className="introduce-table">
-            <p className="text-top">아웃룩을 새롭게</p>
-            <p className="text-bottom">더 가치 있게 </p>
-            <span>
-              토틀은 Outlook에서 부족한 부분들을 채워주는
-              <br />
-              올인원 아웃룩 애드인 솔루션입니다.
-            </span>
-          </div>
-          <div className="email-table">
-            <input
-              className="email-input"
-              placeholder="이메일 주소를 입력해주세요."
-            ></input>
-            <button className="email-button">계정 생성하기</button>
-            <p className="email-text">
-              무료 계정을 생성해 토틀의 다양한 기능을 활용해보세요!
-            </p>
-          </div>
-          <div className="subscribe">→ 토틀 구독하기</div>
-        </div>
-        <div className="video-wrapper">
-          <WistiaPlayer
-            videoId="4pzjgbcafv"
-            wrapper="wistia-player-container-1"
-          />
-        </div>
-      </div>
+      <SectionTop></SectionTop>
       <div className="product-wrapper">
         <div className="product-text">
           <p className="t1">Product Tour 👇</p>
