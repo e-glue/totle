e2f7253954c19b281c39398cfb19168781d340bc
zooooss <wldn000829@gmail.com>
2023-08-28 14:52:00 +0900

    release-features

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const { override, addWebpackModuleRule } = require("customize-cra");
+
+module.exports = override(
+  addWebpackModuleRule({
+    test: /\.(png|jpg|gif)$/i,
+    type: 'asset',
+    parser: {
+      dataUrlCondition: {
+        maxSize: 4 * 1024 * 1024 // 4MB
+      }
+    }
+  })
+);
--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import Introduction from "./pages/introduction/Introduction";
 import Features from "./pages/features/Features";
-import Pricing from "./pages/pricing/Pricing";
-import Contact from "./pages/contact/Contact";
 import "./styles/styles.scss";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Introduction />} />
-      <Route path="/features" element={<Features />} />
-      <Route path="/pricing" element={<Pricing />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <Features />
   );
 }
 
